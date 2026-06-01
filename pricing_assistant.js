4bdc28b3a7bb4e7ec7bb220dcd77bd70048b37cc
Sohil Desai <sohildesai@usf.edu>
2026-05-31 22:15:48 -0400

    Add console output

--- pricing_assistant.js
+++ pricing_assistant.js
@@ -12,3 +12,9 @@ let breakEvenUnits = profitPerUnit > 0
     : "Not profitable";
 
 let isProfitablePerUnit = profitPerUnit > 0;
+console.log("Product Name:", productName);
+console.log("Discounted Price (Before Tax): $" + discountedPrice.toFixed(2));
+console.log("Final Price With Tax: $" + finalPriceWithTax.toFixed(2));
+console.log("Profit Per Unit: $" + profitPerUnit.toFixed(2));
+console.log("Break-Even Units:", breakEvenUnits);
+console.log("Per-Unit Profitability:", isProfitablePerUnit);
