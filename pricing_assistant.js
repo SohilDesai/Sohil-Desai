13d3c1c63dbd69d825ad0aa01067e7d29f5e6941
Sohil Desai <sohildesai@usf.edu>
2026-05-31 22:00:02 -0400

    Add pricing calculations

--- pricing_assistant.js
+++ pricing_assistant.js
@@ -4,3 +4,6 @@ let basePrice = 79.99;
 let discountRate = 0.15;
 let salesTaxRate = 0.07;
 let fixedMonthlyCosts = 500.00;
+let discountedPrice = basePrice * (1 - discountRate);
+let finalPriceWithTax = discountedPrice * (1 + salesTaxRate);
+let profitPerUnit = finalPriceWithTax - costPerUnit;
